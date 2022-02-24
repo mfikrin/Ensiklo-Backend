c235c7285ad0f477347cdf336526141fdb6f1c20
Nabelanita <utami.nabelanita@gmail.com>
2022-02-24 08:51:10 +0700

    add register base files

---
+++ PPL/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PPL.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+    }
+}
