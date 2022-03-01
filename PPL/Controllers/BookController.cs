5ece057ec431843ccf5719a376a6f0eefce4a29c
mfikrin <muhammad.fikrin1729@gmail.com>
2022-03-01 16:44:34 +0700

    fix : update query get books

--- PPL/Controllers/BookController.cs
+++ PPL/Controllers/BookController.cs
@@ -28,13 +28,7 @@ namespace PPL.Controllers
         public JsonResult Get()
         {
             string query = @"
-                SELECT b.id_book,title,rating,description_book,pages,publisher,url_cover,array_to_string(array_agg(concat(a.author_first_name,' ',a.author_last_name) order by ba.author_ordinal ASC), ', ') AS author_names
-                FROM books b
-                LEFT JOIN books_authors ba
-                  ON (b.id_book = ba.id_book)
-                LEFT JOIN authors a 
-                  ON (ba.id_author = a.id_author)
-                GROUP BY b.id_book
+               SELECT * FROM books
             ";
 
             DataTable table = new DataTable();
@@ -61,56 +55,5 @@ namespace PPL.Controllers
 
         }
 
-        //// GET api/<BookController>/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/<BookController>
-        //[HttpPost]
-        //public ActionResult Post([FromBody] Book newBook)
-        //{
-        //    bool badThingHappened = false;
-        //    if (badThingHappened)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    return Created("", newBook);
-        //}
-
-        //// PUT api/<BookController>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //    //bool badThingHappened = false;
-        //    //if (badThingHappened)
-        //    //{
-        //    //    return BadRequest();
-        //    //}
-                
-        //    //return Created("",)
-            
-
-        //}
-
-        //// DELETE api/<BookController>/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //    //bool badThingsHappened = false;
-
-        //    //if (badThingsHappened)
-        //    //{
-        //    //    return BadRequest();
-        //    //}
-
-
-
-        //    //return NoContent();
-            
-        //}
     }
 }
--- PPL/Models/Book.cs
+++ PPL/Models/Book.cs
@@ -2,12 +2,15 @@
 {
     public class Book
     {
-        public string idBook { get; set; }
+        public int id_book { get; set; }
         public string title { get; set; }
-        public int rating { get; set; }
-        public string description_book { get; set; }
+        public string author { get; set; }
         public string publisher { get; set; }
+        public int yearPublished { get; set; }
+        public string description_book { get; set; }
+        public string bookContent { get; set; }
         public string url_cover { get; set; }
-        public string[] author { get; set; }
+        public int catID { get; set; }
+        public string keywords { get; set; }
     }
 }
