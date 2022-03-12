0b29c1b1fa5df41aff73ce5ddb1f93c8c7c7c867
mfikrin <muhammad.fikrin1729@gmail.com>
2022-03-12 11:26:55 +0700

    feat : update book model and launchsetting.json

--- PPL/Models/Book.cs
+++ PPL/Models/Book.cs
@@ -6,11 +6,12 @@
         public string title { get; set; }
         public string author { get; set; }
         public string publisher { get; set; }
-        public int yearPublished { get; set; }
+        public string year_published { get; set; }
         public string description_book { get; set; }
-        public string bookContent { get; set; }
+        public string book_content { get; set; }
         public string url_cover { get; set; }
-        public int catID { get; set; }
+        public string category { get; set; }
         public string keywords { get; set; }
+
     }
 }
