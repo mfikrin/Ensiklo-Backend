81b7b0045e47d3d6a106ab7e632c99e2e8a8e0db
mfikrin <muhammad.fikrin1729@gmail.com>
2022-03-31 01:28:06 +0700

    feat : add sorting book API feature

--- PPL/Controllers/BookController.cs
+++ PPL/Controllers/BookController.cs
@@ -4,6 +4,7 @@ using Npgsql;
 using PPL.Models;
 using System;
 using System.Data;
+using System.Diagnostics;
 
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,14 +15,14 @@ namespace PPL.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
-        
+
         private readonly IConfiguration _configuration;
 
         public BookController(IConfiguration configuration)
         {
             _configuration = configuration;
         }
-        
+
 
         // GET: api/<BookController>
         [HttpGet]
@@ -90,15 +91,13 @@ namespace PPL.Controllers
         }
 
 
-
-
         [HttpPost]
         public JsonResult Post(Book book)
         {
             string query = @"
                 INSERT INTO 
-                books(title,author,publisher,year_published,description_book,book_content,url_cover,category,keywords)
-                values (@title,@author,@publisher,@year_published,@description_book,@book_content,@url_cover,@category,@keywords)
+                books(title,author,publisher,year_published,description_book,book_content,page,url_cover,category,added_time,keywords)
+                values (@title,@author,@publisher,@year_published,@description_book,@book_content,@page,@url_cover,@category,@added_time,@keywords)
             ";
 
             DataTable table = new DataTable();
@@ -109,14 +108,24 @@ namespace PPL.Controllers
                 myCon.Open();
                 using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
                 {
-                    myCommand.Parameters.AddWithValue("@title", book.Title);
+                    myCommand.Parameters.AddWithValue("@title", book.Title.ToLower());
                     myCommand.Parameters.AddWithValue("@author", book.Author);
                     myCommand.Parameters.AddWithValue("@publisher", book.Publisher);
+
+                    //DateOnly dateOnly = DateOnly.FromDateTime(DateTime.Now);
+                    //DateOnly date = new DateOnly(2000, 3, 10);
+
                     myCommand.Parameters.AddWithValue("@year_published", book.Year_published);
                     myCommand.Parameters.AddWithValue("@description_book", book.Description_book);
                     myCommand.Parameters.AddWithValue("@book_content", book.Book_content);
+                    myCommand.Parameters.AddWithValue("@page", book.Page);
                     myCommand.Parameters.AddWithValue("@url_cover", book.Url_cover);
                     myCommand.Parameters.AddWithValue("@category", book.Category);
+
+                    //DateTime time = DateTime.Now;
+                    //Debug.WriteLine(time);
+
+                    myCommand.Parameters.AddWithValue("@added_time", book.Added_time);
                     myCommand.Parameters.AddWithValue("@keywords", book.Keywords);
 
                     myReader = myCommand.ExecuteReader();
@@ -143,8 +152,10 @@ namespace PPL.Controllers
                 year_published = @year_published,
                 description_book = @description_book,
                 book_content = @book_content,
+                page = @page,
                 url_cover = @url_cover,
                 category = @category,
+                added_time = @added_time,
                 keywords = @keywords
 
                 WHERE id_book=@id_book 
@@ -158,15 +169,16 @@ namespace PPL.Controllers
                 myCon.Open();
                 using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
                 {
-                    myCommand.Parameters.AddWithValue("@id_book", book.Id_book);
-                    myCommand.Parameters.AddWithValue("@title", book.Title);
+                    myCommand.Parameters.AddWithValue("@title", book.Title.ToLower());
                     myCommand.Parameters.AddWithValue("@author", book.Author);
                     myCommand.Parameters.AddWithValue("@publisher", book.Publisher);
                     myCommand.Parameters.AddWithValue("@year_published", book.Year_published);
                     myCommand.Parameters.AddWithValue("@description_book", book.Description_book);
                     myCommand.Parameters.AddWithValue("@book_content", book.Book_content);
+                    myCommand.Parameters.AddWithValue("@page", book.Page);
                     myCommand.Parameters.AddWithValue("@url_cover", book.Url_cover);
                     myCommand.Parameters.AddWithValue("@category", book.Category);
+                    myCommand.Parameters.AddWithValue("@added_time", book.Added_time);
                     myCommand.Parameters.AddWithValue("@keywords", book.Keywords);
                     myReader = myCommand.ExecuteReader();
                     table.Load(myReader);
@@ -208,6 +220,137 @@ namespace PPL.Controllers
             }
 
             return new JsonResult("Deleted Successfully");
+        }
+
+        [HttpGet]
+        [Route("search")]
+        
+        public JsonResult Search([FromQuery(Name = "title")] string title)
+        {
+            string query = @$"
+                SELECT *
+                FROM books b
+                WHERE b.title LIKE '%{title}%'
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EnsikloAppCon");
+            NpgsqlDataReader dataReader;
+
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    dataReader = myCommand.ExecuteReader();
+                    table.Load(dataReader);
+
+                    dataReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet]
+        [Route("sort/title")]
+        
+        public JsonResult SortTitle()
+        {
+            string query = @"
+               SELECT * FROM books b order by b.title
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EnsikloAppCon");
+            NpgsqlDataReader dataReader;
+
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    dataReader = myCommand.ExecuteReader();
+                    table.Load(dataReader);
+
+                    dataReader.Close();
+                    myCon.Close();
+
+                }
+            }
+
+
+
+            return new JsonResult(table);
+
+        }
+
+        [HttpGet]
+        [Route("sort/AddedTime")]
+        public JsonResult SortAddedTime()
+        {
+            string query = @"
+               SELECT * FROM books b order by b.added_time
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EnsikloAppCon");
+            NpgsqlDataReader dataReader;
+
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    dataReader = myCommand.ExecuteReader();
+                    table.Load(dataReader);
+
+                    dataReader.Close();
+                    myCon.Close();
+
+                }
+            }
+
+
+
+            return new JsonResult(table);
+
+        }
+
+
+        [HttpGet]
+        [Route("sort/LastRead/{id}")]
+        
+        public JsonResult SortLastRead(int id)
+        {
+            string query = @"
+               SELECT * FROM books natural join library_user where id_user = @id_user
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EnsikloAppCon");
+            NpgsqlDataReader dataReader;
+
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@id_user", id);
+                    dataReader = myCommand.ExecuteReader();
+                    table.Load(dataReader);
+
+                    dataReader.Close();
+                    myCon.Close();
+
+                }
+            }
+
+
+
+            return new JsonResult(table);
+
         }
 
     }
