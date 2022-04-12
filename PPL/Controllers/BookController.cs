086d227ebeb3bfa06b610ac299d1df7492c9e82a
mfikrin <muhammad.fikrin1729@gmail.com>
2022-04-12 12:25:15 +0700

    feat : add BE most popular book from all user in their library

--- PPL/Controllers/BookController.cs
+++ PPL/Controllers/BookController.cs
@@ -396,6 +396,43 @@ namespace PPL.Controllers
 
         }
 
+        [HttpGet]
+        [Route("MostPopular/{limit}")]
+        public JsonResult GetMostPopular(int limit)
+        {
+            string query = @"
+                SELECT id_book,title,author, publisher, year_published, description_book, book_content, url_cover, category, keywords, added_time, page
+                FROM books b natural join library_user lu 
+                GROUP BY id_book 
+                ORDER BY COUNT(id_book) DESC
+                LIMIT @limit
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EnsikloAppCon");
+            NpgsqlDataReader dataReader;
+
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@limit", limit);
+                    dataReader = myCommand.ExecuteReader();
+                    table.Load(dataReader);
+
+                    dataReader.Close();
+                    myCon.Close();
+
+                }
+            }
+
+
+
+            return new JsonResult(table);
+
+        }
+
 
 
     }
